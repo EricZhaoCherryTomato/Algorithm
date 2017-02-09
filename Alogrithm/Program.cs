63dc466acba91c01d5e775d96dd191f8cb4e8ce0
Eric.Zhao <Eric.Zhao@dominos.com.au>
2017-02-09 14:30:01 +1000

    GCD

---
+++ Alogrithm/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Alogrithm
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine(Gcd(3, 6));
+            Console.ReadKey(true);
+        }
+
+        public static int Gcd(int p, int q)
+        {
+            if (q == 0)
+            {
+                return p;
+            }
+            //3 divided by 7 is 0 with a remainder of 3
+            var r = p % q;
+            return Gcd(q, r);
+        }
+    }
+}
