c8d8a0fd676df9662cb26d9f9dfe3472c19cc0fb
Eric.Zhao <Eric.Zhao@dominos.com.au>
2017-02-13 09:58:50 +1000

    Inheritance experiment not part of Algorithm

---
+++ Inheritance/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal a = new Animal();
+            Animal b = new Dog();
+
+            a.Move();
+            b.Move();
+            //b.Bark();
+            Console.ReadKey(true);
+        }
+    }
+
+    class Animal
+    {
+        public virtual void Move()
+        {
+            Console.WriteLine("A move");
+        }
+    }
+
+    class Dog : Animal
+    {
+
+        public override void Move()
+        {
+            Console.WriteLine("D Move");
+        }
+        public void Bark()
+        {
+            Console.WriteLine("D bark");
+        }
+    }
+}
---
+++ JavaAlgorithm/test/my-app/src/main/java/app/myalgorithm/com/App.java
@@ -0,0 +1,13 @@
+package app.myalgorithm.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
